2ca65223e196a3933d2b614fdc5270e4790b7f16
Brandon <btj010@email.latech.edu>
2024-04-12 17:56:09 -0500

    Created bones

---
+++ game.py
@@ -0,0 +1,12 @@
+import pygame
+
+pygame.init()
+
+WIDTH, HEIGHT = 800, 600
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Game Jam Game")
+
+RUNNING = True
+
+while RUNNING:
+    window.blit()
