25b5b0ae00fdb97ac318c4b05be4646a78079660
Brandon <btj010@email.latech.edu>
2024-04-12 18:13:02 -0500

    ok its actually a window now

--- game.py
+++ game.py
@@ -2,11 +2,20 @@ import pygame
 
 pygame.init()
 
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 1000, 600
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Game Jam Game")
 
 RUNNING = True
 
+rect = pygame.surface.Surface((50, 50))
+
 while RUNNING:
-    window.blit()
+    window.fill((255,255,255))
+
+    window.blit(rect, (0,0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            RUNNING = False
+    pygame.display.update()
