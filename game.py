a2e65600fcc717144ec26d075ceb066baa1926e3
Brandon <btj010@email.latech.edu>
2024-04-12 19:04:27 -0500

    Created tile-based system

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, os, json
 
 pygame.init()
 
@@ -6,14 +6,59 @@ WIDTH, HEIGHT = 1000, 600
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Game Jam Game")
 
-RUNNING = True
+TILE_SIZE = 50
 
 rect = pygame.surface.Surface((50, 50))
+
+class World:
+    def __init__(self, world_data):
+        self.tiles = []
+
+        current_row = 0
+        for row in world_data:
+            current_col = 0
+            for tile in row:
+                if "floor" in tile:
+                    img_rect = pygame.image.load(f"images\\{tile}.png").get_rect()
+                    img_rect.topleft = (current_col * TILE_SIZE, current_row * TILE_SIZE)
+                    tile = Tile(pygame.image.load(f"images\\{tile}.png"), img_rect, False)
+                else:
+                    img_rect = pygame.image.load(f"images\\{tile}.png").get_rect()
+                    img_rect.topleft = (current_col * TILE_SIZE, current_row * TILE_SIZE)
+                    tile = Tile(pygame.image.load(f"images\\{tile}.png"), img_rect, True)
+                self.tiles.append(tile)
+                current_col += 1
+            current_row += 1
+
+    def draw(self):
+        for tile in self.tiles:
+            window.blit(tile.image, tile.rect)
+
+class Tile:
+    def __init__(self, image, rect, collision):
+        self.image = image
+        self.rect = rect
+        self.collision = collision
+
+
+    def displayTiles(self):
+        for tile in self.tiles:
+            window.blit(tile[0], tile[1])
+
+if os.path.exists('level_data.txt'):
+    level_file = open('level_data.txt', 'r')
+    world_data = json.load(level_file)
+    world = World(world_data)
+    RUNNING = True
+else:
+    RUNNING = False
+    print("not all files downloaded :(")
 
 while RUNNING:
     window.fill((255,255,255))
 
     window.blit(rect, (0,0))
+    world.draw()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
