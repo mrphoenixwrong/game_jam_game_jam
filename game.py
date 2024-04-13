bd38d734d522a460944308bffe7642407b126374
Brandon <btj010@email.latech.edu>
2024-04-12 21:36:03 -0500

    Added testing images, player class, and player movement

--- game.py
+++ game.py
@@ -21,25 +21,16 @@ class World:
             current_col = 0
             for tile in row:
                 if "floor" in tile:
-                    img_rect = pygame.image.load(f"images\\{tile}.png").get_rect()
-                    img_rect.topleft = (current_col * TILE_SIZE, current_row * TILE_SIZE)
-                    tile = Tile(pygame.image.load(f"images\\{tile}.png"), False)
+                    tile = Tile(tile, (current_col * TILE_SIZE, current_row * TILE_SIZE), False)
                 else:
-                    img_rect = pygame.image.load(f"images\\{tile}.png").get_rect()
-                    img_rect.topleft = (current_col * TILE_SIZE, current_row * TILE_SIZE)
-                    tile = Tile(pygame.image.load(f"images\\{tile}.png"), True)
+                    tile = Tile(tile, (current_col * TILE_SIZE, current_row * TILE_SIZE), False)
                 self.tiles.append(tile)
                 current_col += 1
             current_row += 1
 
-    def draw(self):
-        for tile in self.tiles:
-            window.blit(tile.image, tile.rect)
-
-
     def displayTiles(self):
         for tile in self.tiles:
-            window.blit(tile[0], tile[1])
+            window.blit(tile.image, tile.rect)
 
 def createWorld():
     if os.path.exists('level_data.txt'):
@@ -55,11 +46,12 @@ clock = pygame.time.Clock()
 
 def game_loop():
     world, RUNNING = createWorld()
+    player = Player((0,0), False)
 
     while RUNNING:
         dt = clock.tick(60)
 
-        world.draw()
+        world.displayTiles()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -68,13 +60,24 @@ def game_loop():
         key_pressed_is = pygame.key.get_pressed()
         
         if key_pressed_is[K_LEFT] or key_pressed_is[K_a]:
-            pass
+            player.rect.x -= 0.3 * dt
         if key_pressed_is[K_RIGHT] or key_pressed_is[K_d]:
-            pass
+            player.rect.x += 0.3 * dt
         if key_pressed_is[K_UP] or key_pressed_is[K_w]:
-            pass
+            player.rect.y -= 0.3 * dt
         if key_pressed_is[K_DOWN] or key_pressed_is[K_s]:
-            pass
+            player.rect.y += 0.3 * dt
+
+        if player.rect.x < TILE_SIZE:
+            player.rect.x = TILE_SIZE
+        if player.rect.x > WIDTH - 2 * TILE_SIZE:
+            player.rect.x = WIDTH - 2 * TILE_SIZE
+        if player.rect.y < 2 * TILE_SIZE:
+            player.rect.y = 2 * TILE_SIZE
+        if player.rect.y > HEIGHT - 2 * TILE_SIZE:
+            player.rect.y = HEIGHT - 2 * TILE_SIZE
+
+        window.blit(player.image, player.rect)
 
         pygame.display.update()
 
