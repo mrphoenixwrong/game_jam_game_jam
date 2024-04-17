1a8befb9f502e79510b6a06c62eaea29639a7c6c
Brandon <btj010@email.latech.edu>
2024-04-16 20:30:59 -0500

    erign

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from game import title_loop, game_loop, transition_loop
 
 RUNNING = True
 
-levels = [[150, (3, 5), 23, 30, True], [60, (5, 10), 5, 5, False], [80, (4, 8), 10, 10, False], [120, (4, 7), 15, 15, True], [120, (3, 6), 18, 20, True]]
+levels = [[60, (5, 10), 5, 5, False], [80, (4, 8), 10, 10, False], [120, (4, 7), 15, 15, True], [120, (3, 6), 18, 20, True], [150, (3, 5), 23, 30, True]]
 
 while RUNNING:
     if RUNNING:
